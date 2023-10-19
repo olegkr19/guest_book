bf6af422d6e018c89753a73d817a81d1dfb1d696
Kryvosheiev <olegkrkr@gmail.com>
2023-10-20 00:28:08 +0300

    style reset template

--- src/Form/ResetPasswordRequestFormType.php
+++ src/Form/ResetPasswordRequestFormType.php
@@ -14,7 +14,10 @@ class ResetPasswordRequestFormType extends AbstractType
     {
         $builder
             ->add('email', EmailType::class, [
-                'attr' => ['autocomplete' => 'email'],
+                'attr' => [
+                    'class' => 'form-control col-4',
+                    'autocomplete' => 'email'
+                ],
                 'constraints' => [
                     new NotBlank([
                         'message' => 'Please enter your email',
